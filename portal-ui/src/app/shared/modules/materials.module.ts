22abbefbbd6f248b24fb9d1fddf89926e88d0ca8
Alejandro Juarez <alejandro-juarez-ctx@users.noreply.github.com>
2020-12-02 00:11:57 -0800

    Create user list table

--- portal-ui/src/app/shared/modules/materials.module.ts
+++ portal-ui/src/app/shared/modules/materials.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
     exports: [
         MatIconModule,
-        MatToolbarModule
+        MatToolbarModule,
+        MatTableModule,
+        MatButtonModule
     ]
 })
 export class MaterialsModule { }
--- portal-ui/src/app/user-list/user-list.component.ts
+++ portal-ui/src/app/user-list/user-list.component.ts
@@ -6,10 +6,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-list.component.scss']
 })
 export class UserListComponent implements OnInit {
+  displayedColumns = ["firstName", "lastName", "email", "actions"];
+
+  users = [
+    {
+      id: 0,
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'john@doe.com'
+    },
+    {
+      id: 1,
+      firstName: 'Jane',
+      lastName: 'Doeson',
+      email: 'jane@doeson.com'
+    }
+  ]
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  update(id: number): void {
+    const user = this.getUser(id);
+    console.log(`Update: ` + user.firstName);
+  }
+
+  delete(id: number): void {
+    const user = this.getUser(id);
+    console.log(`Delete: ` + user.firstName);
+  }
+
+  private getUser(id: number): any {
+    return Object.values(this.users).filter((x: any) => x.id === id)[0];
   }
 
 }
