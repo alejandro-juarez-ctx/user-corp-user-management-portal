1e8d93232e2141d82c61092c38b8fe564822a751
Alejandro Juarez <alejandro-juarez-ctx@users.noreply.github.com>
2020-12-03 13:25:23 -0800

    Handle get and create users

--- portal-backend/accounts/src/main/java/com/usercorp/accounts/controllers/AccountsController.java
+++ portal-backend/accounts/src/main/java/com/usercorp/accounts/controllers/AccountsController.java
@@ -1,25 +1,32 @@
 package com.usercorp.accounts.controllers;
 
-import io.swagger.annotations.ApiOperation;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.usercorp.accounts.entities.User;
+import com.usercorp.accounts.models.UserCreate;
+import com.usercorp.accounts.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/accounts")
 public class AccountsController {
 
-    @GetMapping("/")
-    @ApiOperation(value = "Get users")
-    public ResponseEntity<List<String>> getUsers() {
-        List<String> users = new ArrayList<>();
-        users.add("test1");
-        users.add("test2");
+    @Autowired
+    private UserRepository userRepository;
 
-        return ResponseEntity.ok().body(users);
+    @GetMapping("/")
+    public @ResponseBody Iterable<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @PostMapping("/")
+    public @ResponseBody String createUser(@RequestBody UserCreate newUser) {
+        User user = new User();
+        user.setFirstName(newUser.getFirstName());
+        user.setLastName(newUser.getLastName());
+        user.setEmail(newUser.getEmail());
+
+        userRepository.save(user);
+
+        return "Saved";
     }
 }
