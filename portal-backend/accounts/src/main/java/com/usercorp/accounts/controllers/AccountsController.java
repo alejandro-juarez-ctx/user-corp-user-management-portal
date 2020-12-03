d8bab9e2644b191d2329ab84939ca044de157c58
Alejandro Juarez <alejandro-juarez-ctx@users.noreply.github.com>
2020-12-03 14:41:47 -0800

    Moved repository related steps to AccountsService

--- portal-backend/accounts/src/main/java/com/usercorp/accounts/controllers/AccountsController.java
+++ portal-backend/accounts/src/main/java/com/usercorp/accounts/controllers/AccountsController.java
@@ -2,8 +2,9 @@ package com.usercorp.accounts.controllers;
 
 import com.usercorp.accounts.entities.User;
 import com.usercorp.accounts.models.RequestUser;
-import com.usercorp.accounts.repositories.UserRepository;
+import com.usercorp.accounts.services.AccountsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -13,58 +14,42 @@ import java.util.Optional;
 public class AccountsController {
 
     @Autowired
-    private UserRepository userRepository;
+    private AccountsService accountsService;
 
     @GetMapping("/")
-    public @ResponseBody Iterable<User> getAll() {
-        return userRepository.findAll();
+    public @ResponseBody ResponseEntity<Iterable<User>> getAll() {
+        Iterable<User> users = accountsService.getAllUsers();
+
+        return ResponseEntity.ok().body(users);
     }
 
     @GetMapping("/{id}")
-    public @ResponseBody User getById(@PathVariable int id) {
-        Optional<User> result = userRepository.findById(id);
+    public @ResponseBody ResponseEntity<User> getById(@PathVariable int id) {
+        Optional<User> result = accountsService.getUserById(id);
 
-        if (result.isEmpty()) return null;
+        if (result.isEmpty()) return ResponseEntity.notFound().build();
 
-        return result.get();
+        User user = result.get();
+        return ResponseEntity.ok().body(user);
     }
 
     @PostMapping("/")
-    public @ResponseBody String create(@RequestBody RequestUser newUser) {
-        User user = new User();
-        user.setFirstName(newUser.getFirstName());
-        user.setLastName(newUser.getLastName());
-        user.setEmail(newUser.getEmail());
-
-        userRepository.save(user);
-
-        return "Saved.";
+    public @ResponseBody ResponseEntity<Object> create(@RequestBody RequestUser user) {
+        accountsService.createUser(user);
+        return ResponseEntity.ok().body(null);
     }
 
     @PutMapping("/")
-    public @ResponseBody String update(@RequestBody User updatedUser) {
-        Optional<User> result = userRepository.findById(updatedUser.getId());
-
-        if (result.isEmpty()) return "No user found with provided id.";
-
-        User user = result.get();
-        user.setEmail(updatedUser.getEmail());
-        user.setFirstName(updatedUser.getFirstName());
-        user.setLastName(updatedUser.getLastName());
-
-        userRepository.save(user);
-
-        return "User updated.";
+    public @ResponseBody ResponseEntity<Object> update(@RequestBody User updatedUser) {
+        return accountsService.update(updatedUser)
+            ? ResponseEntity.ok().build()
+            : ResponseEntity.badRequest().build();
     }
 
     @DeleteMapping("/{id}")
-    public @ResponseBody String delete(@PathVariable int id) {
-        Optional<User> result = userRepository.findById(id);
-
-        if (result.isEmpty()) return "No user found with provided id.";
-
-        userRepository.delete(result.get());
-
-        return "User deleted.";
+    public @ResponseBody ResponseEntity<Object> delete(@PathVariable int id) {
+        return accountsService.delete(id)
+                ? ResponseEntity.ok().build()
+                : ResponseEntity.badRequest().build();
     }
 }
---
+++ portal-backend/accounts/src/main/java/com/usercorp/accounts/services/AccountsService.java
@@ -0,0 +1,57 @@
+package com.usercorp.accounts.services;
+
+import com.usercorp.accounts.entities.User;
+import com.usercorp.accounts.models.RequestUser;
+import com.usercorp.accounts.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AccountsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public Iterable<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(int id) {
+        return userRepository.findById(id);
+    }
+
+    public void createUser(RequestUser newUser) {
+        User user = new User();
+        user.setFirstName(newUser.getFirstName());
+        user.setLastName(newUser.getLastName());
+        user.setEmail(newUser.getEmail());
+
+        userRepository.save(user);
+    }
+
+    public boolean update(User updatedUser) {
+        Optional<User> result = getUserById(updatedUser.getId());
+
+        if (result.isEmpty()) return false;
+
+        User user = result.get();
+
+        user.setFirstName(updatedUser.getFirstName());
+        user.setLastName(updatedUser.getLastName());
+        user.setEmail(updatedUser.getEmail());
+
+        userRepository.save(user);
+        return true;
+    }
+
+    public boolean delete(int id) {
+        Optional<User> result = getUserById(id);
+
+        if (result.isEmpty()) return false;
+
+        userRepository.delete(result.get());
+        return true;
+    }
+}
