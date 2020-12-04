f7e181db50bfb00d05910fee270591b7b387df44
Alejandro Juarez <alejandro-juarez-ctx@users.noreply.github.com>
2020-12-03 22:27:28 -0800

    Added api operation description to endpoints

--- portal-backend/accounts/src/main/java/com/usercorp/accounts/controllers/AccountsController.java
+++ portal-backend/accounts/src/main/java/com/usercorp/accounts/controllers/AccountsController.java
@@ -3,6 +3,7 @@ package com.usercorp.accounts.controllers;
 import com.usercorp.accounts.entities.User;
 import com.usercorp.accounts.models.RequestUser;
 import com.usercorp.accounts.services.AccountsService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +18,7 @@ public class AccountsController {
     private AccountsService accountsService;
 
     @GetMapping("/")
+    @ApiOperation(value = "Get all registered users.")
     public @ResponseBody ResponseEntity<Iterable<User>> getAll() {
         Iterable<User> users = accountsService.getAllUsers();
 
@@ -24,6 +26,7 @@ public class AccountsController {
     }
 
     @GetMapping("/{id}")
+    @ApiOperation(value = "Get user by id.")
     public @ResponseBody ResponseEntity<User> getById(@PathVariable int id) {
         Optional<User> result = accountsService.getUserById(id);
 
@@ -34,12 +37,14 @@ public class AccountsController {
     }
 
     @PostMapping("/")
+    @ApiOperation(value = "Register new user.")
     public @ResponseBody ResponseEntity<Object> create(@RequestBody RequestUser user) {
         accountsService.createUser(user);
         return ResponseEntity.ok().body(null);
     }
 
     @PutMapping("/")
+    @ApiOperation(value = "Update user data.")
     public @ResponseBody ResponseEntity<Object> update(@RequestBody User updatedUser) {
         return accountsService.update(updatedUser)
             ? ResponseEntity.ok().build()
@@ -47,6 +52,7 @@ public class AccountsController {
     }
 
     @DeleteMapping("/{id}")
+    @ApiOperation(value = "Delete user by id.")
     public @ResponseBody ResponseEntity<Object> delete(@PathVariable int id) {
         return accountsService.delete(id)
                 ? ResponseEntity.ok().build()
